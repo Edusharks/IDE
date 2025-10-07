8e2f9a15cc3f69b8a62b19c28f579bba85415843
Edusharks <edusharks.content@gmail.com>
2025-10-07 10:03:48 +0530

    Delete src/renderer/theme-loader.js

--- src/renderer/theme-loader.js
+++
@@ -1,4 +0,0 @@
-// This script's only job is to read the theme from localStorage
-// and apply it to the <html> tag before the rest of the page loads.
-const savedTheme = localStorage.getItem('blockIdeTheme') || 'light';
-document.documentElement.setAttribute('data-theme', savedTheme);
